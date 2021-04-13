eaeef92a1c49c16504eb01ca52fd59ddb2755475
Aravind Reddy P <ponugotiaravind@gmail.com>
2021-04-13 13:30:43 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Trivia Quiz</h3>
+        <section className="info">
+          {questions.map((question) => (
+            <SingleQuestion {...question} key={question.id} />
+          ))}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
