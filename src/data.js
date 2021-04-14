0b5101583a601cdc438364b9c2155cfef726d766
Aravind Reddy P <ponugotiaravind@gmail.com>
2021-04-13 23:32:44 -0400

    initial commit

--- src/data.js
+++ src/data.js
@@ -1,33 +1,28 @@
 const questions = [
   {
     id: 1,
-    title: 'Do I have to allow the use of cookes?',
-    info:
-      'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+    title: 'What is the capital of Czech Republic?',
+    info: 'Prague',
   },
   {
     id: 2,
-    title: 'How do I change my My Page password?',
-    info:
-      'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+    title: 'What is the Population of China?',
+    info: '1.39 Billion',
   },
   {
     id: 3,
-    title: 'What is BankID?',
-    info:
-      'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+    title: 'What is the strongest currency in the World?',
+    info: 'Kuwaiti Dinar (0.3 Kuwaiti Dinar = 1USD)',
   },
   {
     id: 4,
-    title: 'Whose birth number can I use?',
-    info:
-      'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+    title: 'Oldest religion in the World?',
+    info: 'Hinduism',
   },
   {
     id: 5,
-    title: 'When do I recieve a password ordered by letter?',
-    info:
-      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+    title: 'What is the height of Mt. Everest?',
+    info: '8,848 Meters',
   },
 ];
 export default questions;
